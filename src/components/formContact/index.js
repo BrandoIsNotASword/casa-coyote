76c63d31db6bd9b801bcc9ef78056cd0a4e76bd9
brando <brando.perez.pacheco@gmail.com>
2020-08-09 20:46:42 -0500

    static kit react email programmatically

--- src/components/formContact/index.js
+++ src/components/formContact/index.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import { useForm } from '@statickit/react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { useFormik } from 'formik'
+import { useStaticKit } from '@statickit/react'
 
 import {
   Textarea,
@@ -9,10 +11,55 @@ import {
   FormErrorMessage,
   Input,
   Stack,
+  Flex,
+  Text,
 } from '@chakra-ui/core'
 
-function FormContact() {
-  const [state, handleSubmit] = useForm('contactForm')
+function FormContact({ hideOnSuccess }) {
+  const [success, setSuccess] = useState()
+  const client = useStaticKit()
+
+  const formik = useFormik({
+    initialValues: { name: '', email: '', message: '' },
+    validate: (values) => {
+      const errors = {}
+
+      if (!values.email) {
+        errors.email = 'Email is required'
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = 'Email is invalid'
+      }
+
+      if (!values.message) {
+        errors.message = "Message can't be empty"
+      }
+
+      return errors
+    },
+    onSubmit: async (values, { setSubmitting }) => {
+      const res = await client.submitForm('contactForm', {
+        name: values.name,
+        email: values.email,
+        message: values.message,
+      })
+
+      if (res.response.ok) setSuccess(true)
+
+      setSubmitting(false)
+    },
+  })
+
+  if (hideOnSuccess && success) {
+    return (
+      <Flex padding={10} justify="center">
+        <Text fontWeight="bold" fontSize="xl">
+          Thank you for your message. We'll reach you as soon as possible.
+        </Text>
+      </Flex>
+    )
+  }
+
+  const { handleBlur, handleChange, handleSubmit, touched, errors, values, isSubmitting } = formik
 
   return (
     <form onSubmit={handleSubmit}>
@@ -26,33 +73,61 @@ function FormContact() {
         maxWidth={{ lg: '960px' }}
       >
         <FormControl>
-          <FormLabel isRequired htmlFor="name">
-            Name
-          </FormLabel>
-          <Input placeholder="John Matthews" id="name" name="name" />
+          <FormLabel htmlFor="name">Name</FormLabel>
+          <Input
+            placeholder="John Matthews"
+            id="name"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
         </FormControl>
 
-        <FormControl>
+        <FormControl isInvalid={errors.email && touched.email}>
           <FormLabel isRequired htmlFor="email">
             Email
           </FormLabel>
-          <Input placeholder="myemail@example.com" id="email" type="email" name="email" />
-          <FormErrorMessage></FormErrorMessage>
+          <Input
+            placeholder="myemail@example.com"
+            id="email"
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <FormErrorMessage>{errors.email}</FormErrorMessage>
         </FormControl>
 
-        <FormControl>
+        <FormControl isInvalid={errors.message && touched.message}>
           <FormLabel isRequired htmlFor="message">
             Message
           </FormLabel>
-          <Textarea name="message" id="message" />
+          <Textarea
+            name="message"
+            id="message"
+            value={values.message}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <FormErrorMessage>{errors.message}</FormErrorMessage>
         </FormControl>
 
-        <Button isDisabled={state.submitting} type="submit" variantColor="primary" size="lg">
+        <Button isDisabled={isSubmitting} type="submit" variantColor="primary" size="lg">
           Send a message
         </Button>
       </Stack>
     </form>
   )
+}
+
+FormContact.propTypes = {
+  hideOnSuccess: PropTypes.bool,
+}
+
+FormContact.defaultProps = {
+  hideOnSuccess: true,
 }
 
 export default FormContact
